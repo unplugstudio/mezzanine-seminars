5490015573e3c0e0e4ca84a0e6e2296e1b40983d
Esperanza Dueñas <esperanzaduenas007@gmail.com>
2021-03-08 00:12:56 -0600

    feat(surveys): export seminars in csv format

--- mezzanine_seminars/admin/surveys.py
+++ mezzanine_seminars/admin/surveys.py
@@ -1,9 +1,16 @@
 from __future__ import unicode_literals, absolute_import
 
+import unicodecsv as csv
 
+from copy import deepcopy
+
+from django.conf.urls import url
 from django.contrib import admin
+from django.utils.module_loading import import_string
+from django.shortcuts import get_list_or_404, HttpResponse
 
 from mezzanine.core.admin import TabularDynamicInlineAdmin
+from mezzanine.conf import settings
 
 from ..models import SurveyResponse, QuestionResponse
 
@@ -42,3 +49,30 @@ class SurveyResponseAdmin(admin.ModelAdmin):
 
     def has_add_permission(self, *args, **kwargs):
         return False  # Don't allow adding through the admin
+
+    def get_urls(self, *args, **kwargs):
+        return [
+            url(
+                r"export/",
+                self.admin_site.admin_view(self.export_surveys),
+                name="mezzanine_seminars_surveys_export",
+            )
+        ] + super(SurveyResponseAdmin, self).get_urls(*args, **kwargs)
+
+    def export_surveys(self, request):
+        """
+        Admin view that generates a CSV export of all surveys response
+        """
+        col_name = settings.SURVEYS_EXPORT_CSV_COLUMN_NAMES
+        get_row_data = import_string(settings.SURVEYS_EXPORT_CSV_ROW_DATA)
+        surveys = QuestionResponse.objects.all()
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(
+            "SurveyResponses"
+        )
+        response.write("\ufeff".encode("utf8"))
+        writer = csv.writer(response)
+        writer.writerow(col_name)
+        for survey in surveys:
+            writer.writerow(get_row_data(survey))
+        return response
